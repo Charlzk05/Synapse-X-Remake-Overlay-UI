ef1f43882a47be6536dcd5cb665832ebde59a136
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-10-29 19:15:13 +0800

    Add project files.

---
+++ Synapse X Remake Overlay UI/MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using WeAreDevs_API;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Synapse_X_Remake_Overlay_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ExploitAPI wrd = new ExploitAPI();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect lprect);
+
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool IsIconic(IntPtr hwnd);
+
+        public static string RobloxWindow = "RobloxPlayerBeta";
+        public static int ProcessIndex = 0;
+        public static string ScriptsPath = "./../../Scripts";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetTimer();
+            LoadScripts();
+            LoadSettings();
+            LoadEditorSyntax();
+        }
+
+        private void LoadEditorSyntax()
+        {
+            StreamReader reader = new StreamReader("./Editor/RblxLua.xml");
+            XmlTextReader xmlReader = new XmlTextReader(reader);
+            Editor.SyntaxHighlighting = HighlightingLoader.Load(xmlReader, HighlightingManager.Instance);
+        }
+
+        private void LoadWRD()
+        {
+            if (wrd.IsUpdated() == false)
+            {
+                var msgbox = MessageBox.Show("WeAreDevs is not yet updated, Are you sure you want continue?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Error);
+
+                if (msgbox == MessageBoxResult.No)
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    if (wrd.isAPIAttached() == false)
+                    {
+                        MessageBox.Show("WeAreDevs is not attached, Please try again ...", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        Application.Current.Shutdown();
+                    }
+                    else
+                    {
+                        SetTimer();
+                        LoadScripts();
+                        LoadSettings();
+                    }
+                }
+            }
+            else
+            {
+                if (wrd.isAPIAttached() == false)
+                {
+                    MessageBox.Show("WeAreDevs is not attached, Please try again ...", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    SetTimer();
+                    LoadScripts();
+                    LoadSettings();
+                }
+            }
+        }
+
+        private void LoadSettings()
+        {
+            Editor.Text = Properties.Settings.Default["EditorSave"].ToString();
+        }
+
+        private void LoadScripts()
+        {
+            foreach (var file in Directory.GetFiles(ScriptsPath, "*.txt"))
+            {
+                ScriptBox.Items.Add(Path.GetFileName(file));
+            }
+
+            foreach (var file in Directory.GetFiles(ScriptsPath, "*.lua"))
+            {
+                ScriptBox.Items.Add(Path.GetFileName(file));
+            }
+        }
+
+        public void SetTimer()
+        {
+            this.Opacity = 0.2;
+            DispatcherTimer UIOverlayer = new DispatcherTimer();
+            UIOverlayer.Interval = TimeSpan.Zero;
+            UIOverlayer.Tick += UIOverlayer_Tick;
+            UIOverlayer.Start();
+        }
+
+        private void UIOverlayer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = Process.GetProcessesByName(RobloxWindow)[ProcessIndex];
+                IntPtr hwnd = process.MainWindowHandle;
+                Rect WindowRect = new Rect();
+                GetWindowRect(hwnd, ref WindowRect);
+                this.Top = WindowRect.Top + 50;
+                this.Left = WindowRect.Left + 30;
+
+                if (IsIconic(hwnd) == true)
+                {
+                    this.WindowState = WindowState.Minimized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+            }
+            catch (Exception)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ScriptHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScriptHubWindow scriptHub = new ScriptHubWindow();
+            scriptHub.Show();
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            wrd.SendLuaCScript(Editor.Text);
+        }
+
+        public static bool Collapsed = false;
+        public static double CurrentHeight;
+
+        private void CollapseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Collapsed == false)
+            {
+                Collapsed = true;
+                CollapseButton.Content = "Expand";
+
+                this.ResizeMode = ResizeMode.NoResize;
+                CurrentHeight = this.ActualHeight;
+
+                CircleEase ease = new CircleEase();
+                ease.EasingMode = EasingMode.EaseInOut;
+                DoubleAnimation HeightAnimation = new DoubleAnimation();
+                HeightAnimation.From = this.ActualHeight;
+                HeightAnimation.To = 80;
+                HeightAnimation.EasingFunction = ease;
+                HeightAnimation.Duration = TimeSpan.FromMilliseconds(500);
+                this.BeginAnimation(HeightProperty, HeightAnimation);
+
+                DoubleAnimation OpacityAnimation = new DoubleAnimation();
+                OpacityAnimation.From = 1;
+                OpacityAnimation.To = 0;
+                OpacityAnimation.EasingFunction = ease;
+                OpacityAnimation.Duration = TimeSpan.FromMilliseconds(500);
+
+                Editor.BeginAnimation(OpacityProperty, OpacityAnimation);
+                ScriptBox.BeginAnimation(OpacityProperty, OpacityAnimation);
+            }
+            else
+            {
+                Collapsed = false;
+                CollapseButton.Content = "Collapse";
+
+                this.ResizeMode = ResizeMode.CanResizeWithGrip;
+
+                CircleEase ease = new CircleEase();
+                ease.EasingMode = EasingMode.EaseInOut;
+                DoubleAnimation HeightAnimation = new DoubleAnimation();
+                HeightAnimation.From = 80;
+                HeightAnimation.To = CurrentHeight;
+                HeightAnimation.EasingFunction = ease;
+                HeightAnimation.Duration = TimeSpan.FromMilliseconds(500);
+                this.BeginAnimation(HeightProperty, HeightAnimation);
+
+                DoubleAnimation OpacityAnimation = new DoubleAnimation();
+                OpacityAnimation.From = 0;
+                OpacityAnimation.To = 1;
+                OpacityAnimation.EasingFunction = ease;
+                OpacityAnimation.Duration = TimeSpan.FromMilliseconds(500);
+
+                Editor.BeginAnimation(OpacityProperty, OpacityAnimation);
+                ScriptBox.BeginAnimation(OpacityProperty, OpacityAnimation);
+            }
+        }
+
+        private void RefreshItem_Click(object sender, RoutedEventArgs e)
+        {
+            ScriptBox.Items.Clear();
+            LoadScripts();
+        }
+
+        private void LoadItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScriptBox.SelectedIndex != -1)
+            {
+                string content = File.ReadAllText($"{ScriptsPath}/{ScriptBox.SelectedItem}");
+                Editor.Text = content;
+            }
+        }
+
+        private void ExecuteItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScriptBox.SelectedIndex != -1)
+            {
+                string content = File.ReadAllText($"{ScriptsPath}/{ScriptBox.SelectedItem}");
+                wrd.SendLuaScript(content);
+            }
+        }
+
+        private void Editor_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["EditorSave"] = Editor.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Text = "";
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua|All Files (*.*)|*.*";
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == true)
+            {
+                string content = File.ReadAllText(openFile.FileName);
+                Editor.Text = content;
+            }
+        }
+
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua|All Files (*.*)|*.*";
+            saveFile.RestoreDirectory = true;
+
+            if (saveFile.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFile.FileName, Editor.Text);
+            }
+        }
+    }
+}
---
+++ Synapse X Remake Overlay UI/ScriptHubWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WeAreDevs_API;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Synapse_X_Remake_Overlay_UI
+{
+    /// <summary>
+    /// Interaction logic for ScriptHubWindow.xaml
+    /// </summary>
+    public partial class ScriptHubWindow : Window
+    {
+        ExploitAPI wrd = new ExploitAPI();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect lprect);
+
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool IsIconic(IntPtr hwnd);
+
+        public static string RobloxWindow = "RobloxPlayerBeta";
+        public static int ProcessIndex = 0;
+        static string json = File.ReadAllText("./../ScriptHub.json");
+        JObject value = JObject.Parse(json);
+
+        public ScriptHubWindow()
+        {
+            InitializeComponent();
+            SetTimer();
+            LoadScripts();
+        }
+
+        private void LoadScripts()
+        {
+            foreach (var item in value["Scripts"])
+            {
+                ScriptBox.Items.Add(item["Title"]);
+            }
+        }
+
+        private void SetTimer()
+        {
+            this.Opacity = 0.2;
+            DispatcherTimer UIOverlayer = new DispatcherTimer();
+            UIOverlayer.Interval = TimeSpan.Zero;
+            UIOverlayer.Tick += UIOverlayer_Tick;
+            UIOverlayer.Start();
+        }
+
+        private void UIOverlayer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = Process.GetProcessesByName(RobloxWindow)[ProcessIndex];
+                IntPtr hwnd = process.MainWindowHandle;
+                Rect WindowRect = new Rect();
+                GetWindowRect(hwnd, ref WindowRect);
+                this.Top = WindowRect.Top + 50;
+                this.Left = WindowRect.Right - 412;
+
+                if (IsIconic(hwnd) == true)
+                {
+                    this.WindowState = WindowState.Minimized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+            }
+            catch (Exception)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ScriptBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ScriptBox.SelectedIndex != -1)
+            {
+                ImageBox.Visibility = Visibility.Hidden;
+                StatusBox.Text = "";
+                StatusBox.Visibility = Visibility.Visible;
+                BitmapImage image = new BitmapImage();
+                image.DownloadProgress += Image_DownloadProgress;
+                image.DownloadCompleted += Image_DownloadCompleted;
+                image.BeginInit();
+                image.UriSource = new Uri(value["Scripts"][ScriptBox.SelectedIndex]["Img"].ToString(), UriKind.Absolute);
+                image.EndInit();
+
+                ImageBox.Source = image;
+            }
+        }
+
+        private void Image_DownloadProgress(object sender, DownloadProgressEventArgs e)
+        {
+            StatusBox.Text = $"Loading ... {e.Progress}%";
+        }
+
+        private void Image_DownloadCompleted(object sender, EventArgs e)
+        {
+            ImageBox.Visibility = Visibility.Visible;
+            StatusBox.Visibility = Visibility.Hidden;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            this.Hide();
+            main.Show();
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScriptBox.SelectedIndex != -1)
+            {
+                string content = value["Scripts"][ScriptBox.SelectedIndex]["Content"].ToString();
+                MessageBox.Show(content);
+            }
+        }
+    }
+}
